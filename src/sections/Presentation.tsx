e90e0679df6f30cc55978b19ce46eea79344bc7a
DE SAINT JEAN Nicolas <nicolas.de-saint-jean@etu.univ-lyon1.fr>
2025-07-25 02:02:17 +0200

    commit

--- src/sections/Presentation.tsx
+++ src/sections/Presentation.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import photo from '../img/photo.png';
+import photo from '../img/20250724_1800s51.jpg';
 import './presentation.css';
 import { MDBCard, MDBCardBody, MDBContainer } from "mdb-react-ui-kit";
 
@@ -7,17 +7,12 @@ function Presentation(){
     return(
         <section id="presentation-section">
             <img src={photo}/>
-            <h1 className="presentation-title">Présentation</h1>
+            <h1 className="presentation-title">Tout d'abord parlons de moi</h1>
             <div className="presentation-text">
-                <p>Je m'appelle Nicolas, j'ai 21 ans et je suis actuellement admis en cycle ingénieur à EPITA pour une formation en alternance en développement web.
-                </p>
-                <p>Passionné depuis mon plus jeune âge par l'informatique et les nouvelles technologies, mon objectif
-                    est de devenir développeur web confirmé. J'ai commencé mon aventure au lycée en
-                    seconde avec la
-                    spécialité Informatique et Création Numérique (ICN) dans lequel j'ai commencé à réaliser mes
-                    premiers projets. Depuis mon envie de progresser ne m'a jamais quitté et
-                    j'ai découvert au fil du temps de nouvelles technologies afin de pouvoir mener à bien mes
-                    projets.</p>
+                <p>Né en 2003 à Ecully près de Lyon, j’ai toujours été fasciné par ce que l’on peut créer avec un ordinateur. Très jeune, j’ai découvert le web en bidoullant des pages, puis la 3D en explorant les univers visuels qui me faisaient rêver. Aujourd’hui encore, ce qui me motive c’est de donner vie à des idées, que ce soit à travers une interface web fluide ou une animation 3D qui parle d’elle-même.</p>
+                <p>Après un bac technologique avec une option numérique, j’ai intégré le BUT Informatique de l’IUT Lyon 1, où j’ai découvert le développement web, les bases de données et un peu de modélisation 3D. Mes stages et mon alternance m’ont permis de mettre en pratique ces compétences sur des projets concrets, confirmant mon intérêt pour le web et les technologies visuelles.</p>
+                <p>Après une première année de master en intelligence artificielle, j’ai choisi de me recentrer sur une voie plus technique et professionnalisante, dans mon domaine qui me tiens à coeur. J’ai ainsi été admis à l'ESGI de Lyon en alternance, une école reconnue dans le domaine de l’informatique, afin de poursuivre ma spécialisation dans le développement et les technologies web.</p>
+                <a href="https://1drv.ms/b/c/d7ec73d02b734dfb/EWS3VNWp44NNqn9XbBG1568BWownetE1zLyJEB7tsLUINw?e=ZAHvTg"> {"-> Télécharger mon CV <-"} </a>
             </div>
             <div className="presentation-timeline">
                 <h2>Mon parcours</h2>
@@ -26,9 +21,9 @@ function Presentation(){
                         <div className="timeline left">
                             <MDBCard>
                                 <MDBCardBody className="p-4">
-                                    <h3>2025 - 2028</h3>
+                                    <h3>2025 - 2027</h3>
                                     <p className="mb-0">
-                                        Cycle ingénieur en informatique à EPITA, avec comme majeure Développement web et nouvelles applications en alternance.
+                                        Master en informatique à ESGI Lyon spécialité - Ingénierie du web - en alternance.
                                     </p>
                                 </MDBCardBody>
                             </MDBCard>
@@ -38,7 +33,7 @@ function Presentation(){
                                 <MDBCardBody className="p-4">
                                     <h3>2024 - 2025</h3>
                                     <p className="mb-0">
-                                        Master 1 Informatique à l'Université de Caen Normande, avec comme parcours Intelligence Artificielle et Facteurs Humains.
+                                        Master 1 Informatique à l'Université de Caen Normandie parcours - Intelligence Artificielle et Facteurs Humains -.
                                     </p>
                                 </MDBCardBody>
                             </MDBCard>
@@ -71,10 +66,8 @@ function Presentation(){
                                 <MDBCardBody className="p-4">
                                     <h3>2021 - 2024</h3>
                                     <p className="mb-0">
-                                        Bachelor Universitaire de Technologie (BUT) en Informatique à l'IUT Lyon 1 à
-                                        Villeurbanne site Doua pour une
-                                        durée de 3 ans. Dès mon entrée en deuxième année, j'ai choisi la spécialité
-                                        Réalisation d'Applications.
+                                        BUT Informatique à l'IUT Lyon 1 parcours
+                                        - Réalisation d'Applications - avec une troisième année en alternance.
                                     </p>
                                 </MDBCardBody>
                             </MDBCard>
@@ -84,8 +77,7 @@ function Presentation(){
                                 <MDBCardBody className="p-4">
                                     <h3>2019 - 2021</h3>
                                     <p className="mb-0">
-                                        Baccalauréat Sciences et Technologies de l'Industrie et du Développement Durable
-                                        (STI2D) spécialité Systèmes d'Information
+                                        Baccalauréat STI2D spécialité - Systèmes d'Information
                                         et Numérique - Mention Assez Bien
                                     </p>
                                 </MDBCardBody>
@@ -96,9 +88,8 @@ function Presentation(){
                                 <MDBCardBody className="p-4">
                                     <h3>2018 - 2019</h3>
                                     <p className="mb-0">
-                                        Spécialité Informatique et Création Numériques (ICN) en classe de seconde, ce
-                                        qui m'a donné l'envie de poursuivre dans
-                                        ce domaine.
+                                        Spécialité Informatique et Création Numériques (ICN) en classe de seconde, qui
+                                        signe le début officiel de mon parcours.
                                     </p>
                                 </MDBCardBody>
                             </MDBCard>
